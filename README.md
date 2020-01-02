99014be92f31fe94928fc2d2837fcbadbe0b5141
Jean-Jacques ETUNE NGI <jeanjacques.etunengi@adservio.fr>
2020-01-02 22:43:06 +0100

    Develop and Test role

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# ansible-role-initserver
+# Ansible Linux based Server Initialization Role
+
+![Python](https://img.shields.io/pypi/pyversions/testinfra.svg?style=flat)
+![Licence](https://img.shields.io/github/license/kube-cloud/ansible-role-initserver.svg?style=flat)
+[![Travis Build](https://img.shields.io/travis/kube-cloud/ansible-role-initserver.svg?style=flat)](https://travis-ci.com/kube-cloud/ansible-role-initserver)
+[![Galaxy Role Downloads](https://img.shields.io/ansible/role/d/41894.svg?style=flat)](https://galaxy.ansible.com/jetune/initserver)
+
 Ansible role for custom KubeCloud Server Initialization 
+
+<a href="https://www.kube-cloud.com/"><img width="250" src="https://kube-cloud.com/images/branding/logo/kubecloud-logo-single_writing_horizontal_color_300x112px.png" /></a>
+<a href="https://www.redhat.com/fr/technologies/management/ansible"><img width="300" src="https://getvectorlogo.com/wp-content/uploads/2019/01/red-hat-ansible-vector-logo.png" /></a>
+
+# Supported OS
+
+* CentOS 6/7
+* RedHat 6/7
+* Ubuntu Xenial/Bionic
+
+# Usage
+
+* Install Role ``` ansible-galaxy install jetune.initserver ```
+* use in your playbook
+```
+---
+- hosts: all
+
+  roles:
+   
+   - role: jetune.initserver
+     vars:
+      server_app_code: "kis"
+      server_password_auth: true
+      server_app_description: "KubeCloud Application description"
+      server_app_home: "/kubecloud/myapp/"
+      server_user_name: "admin"
+      server_user_comment: "KIS Application Administrator"
+      server_user_password: "admin123"
+      server_user_groups: ["kis", "sudo", "adm", "exploit"]
+      server_user_create_home: true
+      server_user_shell: "/usr/bin/bash"
+      server_user_ssh_private_key: "security/admin_rsa"
+      server_user_autorized_keys: ["security/admin_rsa.pub"]
+
+```
---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,76 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_heketi_installed(host):
+
+    # Application code
+    server_app_code = 'kis'
+
+    # System Release
+    server_app_home = '/kubecloud/myapp/' + server_app_code
+
+    # App Logs Dir
+    server_app_logs = server_app_home + '/logs'
+
+    # App Config dir
+    server_app_configuration = server_app_home + '/configuration'
+
+    # App Security dir
+    server_app_security = server_app_home + '/security'
+
+    # App Script dir
+    server_app_sccipts = server_app_home + '/scripts'
+
+    # App Delivery dir
+    server_app_delivery = server_app_home + '/delivery'
+
+    # App Datas dir
+    server_app_datas = server_app_home + '/datas'
+
+    # App Username dir
+    server_user_name = 'admin'
+
+    # Private key
+    server_user_ssh_private_key = '/home/' + server_user_name + '/.ssh/id_rsa'
+
+    # Check home dir
+    assert host.file(server_app_home).exists
+    assert host.file(server_app_home).is_directory
+
+    # Check conf dir
+    assert host.file(server_app_configuration).exists
+    assert host.file(server_app_configuration).is_directory
+
+    # Check logs dir
+    assert host.file(server_app_logs).exists
+    assert host.file(server_app_logs).is_directory
+
+    # Check security dir
+    assert host.file(server_app_security).exists
+    assert host.file(server_app_security).is_directory
+
+    # Check script dir
+    assert host.file(server_app_sccipts).exists
+    assert host.file(server_app_sccipts).is_directory
+
+    # Check delivery dir
+    assert host.file(server_app_delivery).exists
+    assert host.file(server_app_delivery).is_directory
+
+    # Check delivery dir
+    assert host.file(server_app_datas).exists
+    assert host.file(server_app_datas).is_directory
+
+    # Check private key
+    assert host.file(server_user_ssh_private_key).exists
+    assert host.file(server_user_ssh_private_key).is_file
+
+    # Check user exists
+    assert host.user(server_user_name).exists
+    assert server_user_name in host.user(server_user_name).groups
+    assert 'sudo' in host.user(server_user_name).groups
