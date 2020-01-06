fd0a8c4eeb5ffa23d244f70f520366b4b904d88a
Jean-Jacques ETUNE NGI <jeanjacques.etunengi@adservio.fr>
2020-01-06 22:57:41 +0100

    Ajout des tests de ownership

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -41,34 +41,42 @@ def test_heketi_installed(host):
     # Check home dir
     assert host.file(server_app_home).exists
     assert host.file(server_app_home).is_directory
+    assert host.file(server_app_home).user == server_user_name
 
     # Check conf dir
     assert host.file(server_app_configuration).exists
     assert host.file(server_app_configuration).is_directory
+    assert host.file(server_app_configuration).user == server_user_name
 
     # Check logs dir
     assert host.file(server_app_logs).exists
     assert host.file(server_app_logs).is_directory
+    assert host.file(server_app_logs).user == server_user_name
 
     # Check security dir
     assert host.file(server_app_security).exists
     assert host.file(server_app_security).is_directory
+    assert host.file(server_app_security).user == server_user_name
 
     # Check script dir
     assert host.file(server_app_sccipts).exists
     assert host.file(server_app_sccipts).is_directory
+    assert host.file(server_app_sccipts).user == server_user_name
 
     # Check delivery dir
     assert host.file(server_app_delivery).exists
     assert host.file(server_app_delivery).is_directory
+    assert host.file(server_app_delivery).user == server_user_name
 
     # Check delivery dir
     assert host.file(server_app_datas).exists
     assert host.file(server_app_datas).is_directory
+    assert host.file(server_app_datas).user == server_user_name
 
     # Check private key
     assert host.file(server_user_ssh_private_key).exists
     assert host.file(server_user_ssh_private_key).is_file
+    assert host.file(server_user_ssh_private_key).user == server_user_name
 
     # Check user exists
     assert host.user(server_user_name).exists
